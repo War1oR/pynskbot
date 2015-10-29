6440c00d0d1e804d824a1a09490746a4a3b36bef
War1oR <warhome@yandex.ru>
2015-10-29 18:14:26 +0300

    ~ command refactoring

--- command.py
+++ command.py
@@ -1,6 +1,13 @@
 __author__ = 'warior'
-__date__ = '28.10.2015'
+__date__ = '29.10.2015'
 
+
+def start(bot, message, members):
+    bot.sendMessage(chat_id=message.chat_id,
+                    text="Hello {}! In order to subscribe to the newsletter, you should "
+                         "enter /sub. In order to unsubscribe from the newsletter, you "
+                         "should enter /unsub.".format(message.chat.first_name))
+    return members
 
 def sub(bot, message, members):
     if message.chat_id in members:
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tornado.ioloop
 from bot import Bot
-from command import cmd
+from command import start, sub, unsub
 from variables import AUTH_TOKEN
 
 __author__ = 'warior'
@@ -8,6 +8,11 @@ __date__ = '27.10.2015'
 
 
 print("start")
+cmd = {"/start": start,
+       "/help": start,
+       "/sub": sub,
+       "/unsub": unsub
+       }
 pynsk_bot = Bot(cmd, AUTH_TOKEN)
 period = tornado.ioloop.PeriodicCallback(pynsk_bot.message_loop, 200)
 period.start()
